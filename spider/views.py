d0cbb9d30d21d2502ca671563026415fcb2b1710
Chahkiev <chahkiev98@mail.ru>
2019-08-29 18:00:43 +0300

    addede logger

--- spider/views.py
+++ spider/views.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import ast
+import datetime
 from tarantool import Connection
 
 from django.shortcuts import render
@@ -10,6 +11,7 @@ from django.views import View
 
 def correct_body(body):
     print(body)
+    logger("correct_body()", body)
     try:
         body = body.decode("utf-8")
         body = ast.literal_eval(body)
@@ -20,6 +22,7 @@ def correct_body(body):
 
 
 def key_exists(key):
+    logger("key_exists()", key)
     c = Connection("127.0.0.1", 3301)
     print("key_exists()")
     exist = list(c.select("KVstorage", key))
@@ -28,6 +31,7 @@ def key_exists(key):
 
 
 def add_kv(key, value):
+    logger("add_kv()", key+" "+value)
     c = Connection("127.0.0.1", 3301)
     print("add_key()")
     try:
@@ -39,6 +43,7 @@ def add_kv(key, value):
 
 
 def update_value(key, value):
+     logger("update_value", key+" "+value)
      c = Connection("127.0.0.1", 3301)
      print("update_value()")
      c.update("KVstorage", key, [('=', 1, value)] )
@@ -46,6 +51,7 @@ def update_value(key, value):
 
 
 def get_key(body):
+    logger("get_key()", body)
     body = body.decode("utf-8")
     body = ast.literal_eval(body)
     key = body['key']
@@ -53,6 +59,7 @@ def get_key(body):
 
 
 def get_value(body):
+    logger("get_body()", body)
     body = body.decode("utf-8")
     body = ast.literal_eval(body)
     value = body['value']
@@ -60,6 +67,7 @@ def get_value(body):
 
 
 def get_kv(key):
+    logger("get_kv()", key)
     c = Connection("127.0.0.1", 3301)
     print("get_kv()")
     print(c.select("KVstorage", key))
@@ -67,9 +75,19 @@ def get_kv(key):
 
 
 def delete_kv(key):
+    logger("delete_kv()", key)
     c = Connection("127.0.0.1", 3301)
     print("delete_kv()")
     c.delete("KVstorage", key)
+    return
+
+
+def logger(func, value):
+    f = open("logfile.txt","a+")
+    now = datetime.datetime.today().strftime("%Y-%m-%d  %H:%M:%S")
+    message = now + " : " + str(func) + "    " + str(value) + "\n"
+    f.write(message)
+    f.close()
     return
 
 
