194a3e460222672f85fd0c74005b51527dd4e2a4
Chahkiev <chahkiev98@mail.ru>
2019-08-29 19:47:58 +0300

    fixed some functions

--- spider/views.py
+++ spider/views.py
@@ -7,31 +7,52 @@ from tarantool import Connection
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+# requestData = {"key" : "test", "value" : {"foo": "bar", "aaa" : "bbb"}}
+# jsn = json.dumps(requestData)
+# r = requests.post('http://localhost:8000/kv/', data  = jsn )
 
 
 def correct_body(body):
     print(body)
     logger("correct_body()", body)
     try:
+        # print(body.json())
         body = body.decode("utf-8")
-        body = ast.literal_eval(body)
+        data = ast.literal_eval(body)
+        # print(body.split("&"))
+        # data = {}
+        # body = body.split("&")
+        # for i in body:
+        #     # print(i)
+        #     tmp = i.split("=")
+        #     data[tmp[0]] = tmp[1]
+
+        print(data)
+        print(data['key'])
+        print(data['value'])
     except:
         return False
-    # return isinstance(body['key'], str) and isinstance(body['value'], dict)
-    return True
+
+    print('key' in data and 'value' in data)
+    return 'key' in data and 'value' in data
+    # return isinstance(data['key'], str) and isinstance(data['value'], dict)
+    # return True
 
 
 def key_exists(key):
     logger("key_exists()", key)
     c = Connection("127.0.0.1", 3301)
     print("key_exists()")
-    exist = list(c.select("KVstorage", key))
-    print(exist != [])
-    return exist != []
+    exists = list(c.select("KVstorage", key))
+    print(exists != [])
+    return exists != []
 
 
 def add_kv(key, value):
-    logger("add_kv()", key+" "+value)
+    print("sdfgh")
+    logger("add_kv()", key + " " + value)
     c = Connection("127.0.0.1", 3301)
     print("add_key()")
     try:
@@ -43,27 +64,31 @@ def add_kv(key, value):
 
 
 def update_value(key, value):
-     logger("update_value", key+" "+value)
+     logger("update_value", key + " " + value)
      c = Connection("127.0.0.1", 3301)
      print("update_value()")
-     c.update("KVstorage", key, [('=', 1, value)] )
+     c.update("KVstorage", str(key), [('=', 1, value)] )
      print(c.select("KVstorage", key))
 
 
 def get_key(body):
     logger("get_key()", body)
     body = body.decode("utf-8")
-    body = ast.literal_eval(body)
-    key = body['key']
-    return key
+    data = ast.literal_eval(body)
+
+    print(data['key'])
+    key = data['key']
+    return str(key)
 
 
 def get_value(body):
     logger("get_body()", body)
     body = body.decode("utf-8")
-    body = ast.literal_eval(body)
-    value = body['value']
-    return value
+    data = ast.literal_eval(body)
+
+    print(data['value'])
+    value = data['value']
+    return str(value)
 
 
 def get_kv(key):
@@ -91,35 +116,39 @@ def logger(func, value):
     return
 
 
+@csrf_exempt
 def kv(request):
     print('JUST KEY-VALUE')
     print(request.body)
-    body = b'{"key": "test", "value": {"foo": "bar"}}'
-    # body = request.body
+    # body = b'{"key": "test", "value": {"foo": "bar"}}'
+    body = request.body
 
     if not correct_body(body):
         print('Parse ERROR')
         return HttpResponse(status=400)
 
-    if request.method == 'GET':  # change to POST
+    if request.method == 'POST':  # change to POST
         key = get_key(body)
+        print("key = ", key)
         value = get_value(body)
-        if key_exists("key777"):  # change for KEY
+        print(key, "--", value)
+        if key_exists(key):  # change for KEY
             return HttpResponse(status=409)
         else:
-            add_kv("key777", "value88005553535") # change for KEY and VALUE
+            add_kv(key, value) # change for KEY and VALUE
             return HttpResponse("Successfully added")
 
     # return render(request, 'spider/kv.html')
 
 
+@csrf_exempt
 def id(request, id):
     print('ID KEY-VALUE')
     print(request.method)
     body = b'{"key": "test", "value": {"foo": "bar"}}'
     # body = request.body
 
-    if request.method == 'GET':   # change to PUT
+    if request.method == 'PUT':   # change to PUT
         if not correct_body(body):
             print('Parse ERROR PUT')
             return HttpResponse(status=400)
@@ -139,7 +168,7 @@ def id(request, id):
             # return render(request, 'spider/id.html', {'json': get_kv("key1")})
             return HttpResponse(get_kv(id))
 
-    elif request.method == 'GET':  # change to DELETE
+    elif request.method == 'DELETE':  # change to DELETE
         if not key_exists("key1"):   # change for id
             return HttpResponse(status=404)
         else:
